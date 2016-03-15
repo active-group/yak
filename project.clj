0a6e26d93f4bd14a64c4d0a9f8f1aea6a2b505d5
David Frese <david.frese@active-group.de>
2016-03-15 16:47:39 +0100

    Add some more project meta info.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yak "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library and tools for integrated localization"
+  :url "https://github.com/active-group/yak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
